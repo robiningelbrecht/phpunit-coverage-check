f9252c09f3fbc5568e51e5ebc101349016c4069e
Robin <ingelbrecht_robin@hotmail.com>
2021-10-17 13:46:32 +0200

    More tests

--- tests/Cli/CoverageCheckerCommandTest.php
+++ tests/Cli/CoverageCheckerCommandTest.php
@@ -5,6 +5,7 @@ namespace Tests\Cli;
 use Ahc\Cli\Input\Parameter;
 use PHPUnit\Framework\TestCase;
 use PHPUnitCoverageChecker\Cli\CoverageCheckerCommand;
+use RuntimeException;
 use Spatie\Snapshots\MatchesSnapshots;
 
 class CoverageCheckerCommandTest extends TestCase
@@ -19,9 +20,19 @@ class CoverageCheckerCommandTest extends TestCase
         $this->assertMatchesJsonSnapshot(array_map(fn (Parameter $argument) => $argument->raw(), $command->allOptions()));
     }
 
-    public function testItShouldGuardValidOptions(): void{
+    public function testItShouldGuardValidFormatter(): void{
         $command = CoverageCheckerCommand::create();
 
-        $command->parse(['bin/coverage-checker','clover.xml', '20', '--formatter=lol']);
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Invalid formatter "test". Valid options are only-percentage, message, no-output');
+        $command->parse(['bin/coverage-checker','clover.xml', '20', '--formatter=test']);
+    }
+
+    public function testItShouldGuardValidProcessor(): void{
+        $command = CoverageCheckerCommand::create();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Invalid processor "test". Valid options are clover-coverage');
+        $command->parse(['bin/coverage-checker','clover.xml', '20', '--processor=test']);
     }
 }
