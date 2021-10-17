01e722f4cde2a3bb21dd04b30d5ef8abdccad25d
Robin <ingelbrecht_robin@hotmail.com>
2021-10-17 13:52:39 +0200

    Unit tests for Command

--- tests/Cli/CoverageCheckerCommandTest.php
+++ tests/Cli/CoverageCheckerCommandTest.php
@@ -20,19 +20,39 @@ class CoverageCheckerCommandTest extends TestCase
         $this->assertMatchesJsonSnapshot(array_map(fn (Parameter $argument) => $argument->raw(), $command->allOptions()));
     }
 
-    public function testItShouldGuardValidFormatter(): void{
+    public function testItShouldParse(): void
+    {
+        $command = CoverageCheckerCommand::create();
+
+        $command->parse([
+           'bin/coverage-checker',
+           '../clover.xml',
+           '20',
+           '--formatter=message',
+           '--processor=clover-coverage',
+           '--exit-on-low-coverage',
+           '--enabled-metrics=one,two,three',
+        ]);
+
+        $this->assertMatchesJsonSnapshot($command->getArgumentValues());
+        $this->assertMatchesJsonSnapshot($command->getOptionValues());
+    }
+
+    public function testItShouldThrowWhenInvalidFormatter(): void
+    {
         $command = CoverageCheckerCommand::create();
 
         $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Invalid formatter "test". Valid options are only-percentage, message, no-output');
-        $command->parse(['bin/coverage-checker','clover.xml', '20', '--formatter=test']);
+        $command->parse(['bin/coverage-checker', 'clover.xml', '20', '--formatter=test']);
     }
 
-    public function testItShouldGuardValidProcessor(): void{
+    public function testItShouldThrowWhenInvalidProcessor(): void
+    {
         $command = CoverageCheckerCommand::create();
 
         $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Invalid processor "test". Valid options are clover-coverage');
-        $command->parse(['bin/coverage-checker','clover.xml', '20', '--processor=test']);
+        $command->parse(['bin/coverage-checker', 'clover.xml', '20', '--processor=test']);
     }
 }
